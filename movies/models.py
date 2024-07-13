60c8ca49c8b5d0eee83126831c88ce996d921075
Adrian Acuña <adriansamacu71@gmail.com>
2024-07-13 12:39:21 -0500

    Examen 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Movies(models.Model):
+    title = models.CharField(max_length=40, null=False)
+    GENERO_TYPE = {
+        ('A', 'Acción'),
+        ('C', 'Comedia'),
+        ('D', 'Drama'),
+        ('I', 'Infantil'),
+        ('T', 'Terror'),
+    }
+    genre = models.CharField(max_length=30, choices=GENERO_TYPE, null=False)
+    movie_director = models.CharField(max_length=60, null=False)
+    publication_year = models.IntegerField(null=False)
+    sinopsis = models.TextField(max_length=250, null=False)
+    
+    def __str__(self) -> str:
+        return self.title
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movies
 
 # Create your views here.
+
+def index(request):
+    movies = Movies.objects.order_by('genre')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def details(request, movie_id):
+    movie = Movies.objects.get(id = movie_id)
+    template = loader.get_template('details.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
+    
